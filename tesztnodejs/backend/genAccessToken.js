c9ddff6205a30e54a90a8a3195b72a80e9294b81
Takacs Rebeka Lili <takacsrebus4@gmail.com>
2025-10-28 19:15:38 +0100

    pop-up for refresh token (doesn't work, will take out. but just in case)

--- tesztnodejs/backend/genAccessToken.js
+++ tesztnodejs/backend/genAccessToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken")
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" })
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1m" })
 }
 module.exports = generateAccessToken
--- tesztnodejs/backend/validateToken.js
+++ tesztnodejs/backend/validateToken.js
@@ -4,6 +4,8 @@ const app = express()
 app.use (express.json())
 const jwt = require("jsonwebtoken")
 const port = process.env.PORT
+const cors = require('cors');
+app.use(cors());
 //We will run this server on a different port i.e. port 5000
 
 app.listen(port,()=> {
@@ -11,29 +13,38 @@ app.listen(port,()=> {
 });
 
 app.get("/posts", validateToken, (req, res)=>{
+  res.set('Access-Control-Allow-Origin', '*');
   console.log("Token is valid");
-  console.log(req.user.user);
-  res.send(`${req.user.user} successfully accessed post`);
+  res.json({tokenValid: true});
 });
 
 function validateToken(req, res, next) {
   //get token from request header
   const authHeader = req.headers["authorization"];
-
   const token = authHeader.split(" ")[1];
   //the request header contains the token "Bearer <token>", split the string and use the second value in the split array.
   
   if (token == null)
-    res.sendStatus(400).send("Token not present");
+    res.sendStatus(400).json({tokenValid : false}); //if there is no token
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
 
     if (err) {
-      res.status(403).send("Token is not valid");
+      res.status(403).json({tokenValid : false});
+      jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) {
+          res.status(403).json({refreshTokenValid : false});
+        }
+        else {
+          res.status(200).json({refreshTokenValid : true});
+        }
+      });
     }
     else {
       req.user = user;
-      next(); //proceed to the next action in the calling function
+      res.token = token;
+      next();
+      //next(); //proceed to the next action in the calling function
     }
   });
-  }
+}
